341e3810170b9a4fc9d63a7a865d420191f49157
Artem Norenko <norenko.artem@gmail.com>
2020-11-16 11:23:33 +0200

    Add project files.

---
+++ ListConcat/CollectionProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ListConcat
+{
+    public static class CollectionProcessor
+    {
+        public static IEnumerable<string> HandleCollection(this ICollection<string> collection, string pattern, int countOption)
+        {
+            var resultCollection = new List<string>();
+            for (var i = 0; i < collection.Count;)
+            {
+                if (collection.ElementAt(i).Contains(pattern))
+                {
+                    if (i + countOption <= collection.Count - 1)
+                    {
+                        resultCollection.Add(string.Concat(collection.Skip(i).Take(countOption)));
+                        i += countOption;
+                    }
+                    else
+                    {
+                        var countToTake = collection.Count - i;
+                        resultCollection.Add(string.Concat(collection.Skip(i).Take(countToTake)));
+                        i += countToTake;
+                    }
+                }
+                else
+                {
+                    resultCollection.Add(collection.ElementAt(i));
+                    i++;
+                }
+            }
+
+            return resultCollection;
+        }
+    }
+}
---
+++ ListConcat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListConcat
+{
+    class Program
+    {
+        static readonly List<string> Collection = new List<string>
+            {
+                "call-log",
+                "call-log",
+                "call-log",
+                "item_04",
+                "item_05",
+                "item_06",
+                "some else data",
+                "call-log",
+                "item_09",
+                "item_10",
+                "string",
+                "call-log",
+                "call-log",
+                "call-log",
+                "call-log",
+            };
+
+        static void Main(string[] args)
+        {
+            // skip atual item and next two
+            var skipNextItems = 3;
+            // pattern to compare
+            var pattern = "call-log";
+
+            var result = Collection.HandleCollection(pattern, skipNextItems);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
